9f24bdada76d8430ecaa2a93de2fa1a100bd4b3a
targetconversation-sysu <56981039+targetconversation-sysu@users.noreply.github.com>
2020-03-02 22:13:51 +0800

    Delete main-vqa.js

--- static/js/main-vqa.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    var timezone = "Etc/GMT" ;
-    var deadline = moment.tz("2020-05-08 23:59:59", timezone);
-    $('#countdown-TOC: Target-Guided Open-Domain Conversation').countdown(deadline.toDate(), function(event) {
-        $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
-    });
-});
